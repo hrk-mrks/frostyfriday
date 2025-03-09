ddbd294deb850500c7d3c078c3c852fcda9284ba
hrk-mrks <haruki.mrks@gmail.com>
2025-03-09 17:04:24 +0900

    修正

---
+++ week41/week41_02-01.sql
@@ -0,0 +1,10 @@
+create or replace table week41 (
+  "STATEMENT1" text,
+  "STATEMENT2" text  
+);
+
+select * from week41;
+insert into week41 values('We', 'Python');
+insert into week41 values('Love', 'Worksheets');
+insert into week41 values('Frosty', 'Are');
+insert into week41 values('Friday', 'Cool');
